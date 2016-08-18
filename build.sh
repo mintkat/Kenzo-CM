b3713d07b4dc59928ba7c280f510671579b58718
MOVZX <movzx@yahoo.com>
2016-08-18 16:00:43 +0700

    Despair™

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="MOVZX"
 export KBUILD_BUILD_HOST="BlackMarch.net"
-export LOCALVERSION="-Despair-or-Darkness"
+export LOCALVERSION="-Despair™"
 
 # Kernel Details
 NAME="Kenzo_Darkness";
